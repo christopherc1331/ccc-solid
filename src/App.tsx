f0f4ce88732681a8cb8c0cbb7891b444f7f3a5f3
Chris <christopherc1331@gmail.com>
2024-02-08 22:42:30 -0600

    added template code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import "./assets/js/modernizr.js";
+import "./assets/js/pace.min.js";
+import "./assets/js/jquery-3.2.1.min.js";
+import "./assets/js/plugins.js";
+import "./assets/js/main.js";
+import "./assets/css/base.css";
+import "./assets/css/vendor.css";
+import "./assets/css/main.css";
+import {Home} from "./Home";
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+      <Home/>
   );
 };
 
---
+++ src/assets/js/index.js
@@ -0,0 +1,7 @@
+import * as jQuery from './jquery-3.2.1.min.js';
+import * as main from './main.js';
+import * as modernizr from './modernizr.js';
+import * as plugins from './plugins.js';
+import * as pace from './pace.min.js';
+
+export default { jQuery, main, modernizr, plugins, pace };
